237d197f2bf4cb0372cdc2b5d0fe6d7d685191b3
Vasiliy Karachev <devlop12x@gmail.com>
2025-12-25 15:52:49 +0300

    Решение ДЗ №1: Квадратные уравнения и ипотека

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
+  let d = b ** 2 - 4 * a * c;
   
-  return arr;
+  if (d < 0) {
+    return [];
+  }
+  
+  if (d === 0) {
+    return [-b / (2 * a)];
+  }
+  
+  return [
+    (-b + Math.sqrt(d)) / (2 * a),
+    (-b - Math.sqrt(d)) / (2 * a)
+  ];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = parseFloat(percent);
+  let c = parseFloat(contribution);
+  let a = parseFloat(amount);
+  let n = parseFloat(countMonths);
+
+  if (isNaN(p) || isNaN(c) || isNaN(a) || isNaN(n)) {
+    return false;
+  }
+
+  let s = a - c;
+  let P = p / 100 / 12;
+  let payment = s * (P + (P / ((Math.pow(1 + P, n)) - 1)));
+  let totalAmount = payment * n;
+
+  return Number(totalAmount.toFixed(2));
 }
